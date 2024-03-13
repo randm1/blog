f8368eb99b2be7b6e0a8d3bafd5843651fb05b1c
Samuel McGarraugh <samuel.bmc@proton.me>
2024-03-12 20:04:41 -0600

    add more blogs to openring, add Dr. Pepper badge bc it is great

--- build-site.sh
+++ build-site.sh
@@ -1,5 +1,7 @@
 openring \
   -s https://joelchrono.xyz/feed.xml \
+  -s https://cool-as-heck.blog/posts_feed \
+  -s https://mediocregopher.com/feed.xml \
   < _includes/in.html \
   > _includes/out.html
 bundle exec jekyll build
