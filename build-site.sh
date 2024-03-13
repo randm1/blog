4029c631f5f1b52218961be80dbb29cfb8beb98f
Samuel McGarraugh <samuel.bmc@proton.me>
2024-03-12 23:08:55 -0600

    update openring blogs

--- build-site.sh
+++ build-site.sh
@@ -1,6 +1,8 @@
 openring \
   -s https://joelchrono.xyz/feed.xml \
   -s https://cool-as-heck.blog/posts_feed \
+  -s https://kevquirk.com/feed \
+  -s https://mikestone.me/feed \
   -s https://mediocregopher.com/feed.xml \
   < _includes/in.html \
   > _includes/out.html
